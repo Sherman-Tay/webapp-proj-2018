ba3de5f390285299d7c094992e2a028910b93aa1
Sherman-Tay <shermantayjy@gmail.com>
2018-10-23 00:11:21 +0800

    Designed the HTML and CSS components of website
    designed a carousel slide deck for index.html , base navigation scheme done.

---
+++ 4717Final_Project/js/slides.js
@@ -0,0 +1,24 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+var interval= setInterval(function(){plusDivs(1);},5000);
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("movie-slides");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
